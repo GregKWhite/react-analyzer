149a06fcdd164f2913a4a1f83506bd83b1fcf5a8
Greg White <gkwhite95@gmail.com>
2022-03-28 16:20:06 -0600

    Remove unused function

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,6 @@ import { Report } from "./types";
 import { parseFile } from "./file-parser";
 import { Project } from "ts-morph";
 
-type StringOrRegexp = string | RegExp;
-type Excludes = StringOrRegexp[] | ((path: string) => boolean);
-
-/**
- * Takes in an array of file paths, and returns an array of file paths that are
- * filtered based on the given `exclude` paths.
- *
- * @param files - an array of file paths
- * @param exclude - an array of strings or regular expressions to be tested,
- * or a function that should return `true` when the path should be excluded.
- * @returns the filtered list of file paths
- */
-function filterFiles(files: string[], exclude: Excludes): string[] {
-  let excludeFn: (path: string) => boolean;
-
-  if (typeof exclude === "function") {
-    excludeFn = exclude;
-  } else {
-    excludeFn = (path: string) => {
-      return exclude.some((stringOrRegexp) => {
-        if (typeof stringOrRegexp === "string") {
-          return path.startsWith(stringOrRegexp);
-        } else {
-          return stringOrRegexp.test(path);
-        }
-      });
-    };
-  }
-
-  return files.filter((path) => !excludeFn(path));
-}
-
 function updateProgress(current: number, total: number) {
   process.stdout.clearLine(0);
   process.stdout.cursorTo(0);
